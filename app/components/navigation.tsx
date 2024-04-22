2173dc3138b8aedfb845e2766d5c36411e6a20b7
by.jo <by.jo@linecorp.com>
2024-04-22 23:58:17 +0900

    add eslint

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -1,13 +1,23 @@
-"use client"
+"use client";
+
 import Link from "next/link";
-import {usePathname} from "next/navigation";
+import { usePathname } from "next/navigation";
 
 export default function Navigation() {
-    const path = usePathname();
-    return (
-        <ul>
-            <li><Link href={'/'}>{`home ${path === '/' ? '👵' : ''}`}</Link></li>
-            <li><Link href={'/about-us'}>{`about-us ${path === '/about-us' ? '👵' : ''}`}</Link></li>
-        </ul>
-    )
+  const path = usePathname();
+  return (
+    <ul>
+      <li>
+        <Link href="/">{`home ${path === "/" ? "👵" : ""}`}</Link>
+      </li>
+      <li>
+        <Link href="/about-us">
+          {`about-us ${path === "/about-us" ? "👵" : ""}`}
+        </Link>
+      </li>
+      <li>
+        <Link href="/movies">{`movies ${path === "/movies" ? "👵" : ""}`}</Link>
+      </li>
+    </ul>
+  );
 }
