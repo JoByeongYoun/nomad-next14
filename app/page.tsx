7df7f64f531aa020afbe7c8db4f235fbc47169da
by.jo <by.jo@linecorp.com>
2024-04-22 22:28:50 +0900

    add navigation layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Navigation from "./components/navigation";
 export default function Tomato() {
     return (
         <div>
-            <Navigation />
             <h1>Home</h1>
         </div>
     )
