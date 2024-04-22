204f291ab49e3c7627e0b0034451ce68b6738eda
by.jo <by.jo@linecorp.com>
2024-04-22 23:58:50 +0900

    add eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Navigation from "./components/navigation";
-
 export default function Tomato() {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
 }
